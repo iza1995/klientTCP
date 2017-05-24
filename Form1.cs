57788c9bf16a7175c22b32c8b67c123aae6df050
student <student@NANO-14>
2017-05-24 09:43:22 +0200

    Powiększono okno i dodano przycisk zamknij

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace klientTCP
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
